2de5eb8560354a2036b623318aaf0938b1a47904
pushpendrasingh3 <149046362+pushpendrasingh3@users.noreply.github.com>
2025-05-12 21:16:45 +0530

    Update selenium_hub_wait_ready.sh

--- cicd/selenium_hub_wait_ready.sh
+++ cicd/selenium_hub_wait_ready.sh
@@ -11,7 +11,6 @@
 #  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
 #
 #  https://www.linkedin.com/in/HariSekhon
-#
 
 set -euo pipefail
 [ -n "${DEBUG:-}" ] && set -x
@@ -20,8 +19,9 @@ srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # shellcheck disable=SC1090,SC1091
 #. "$srcdir/lib/utils.sh"
 #./../lib/utils.sh
-ls -la DevOps-Bash-tools
+#ls -la DevOps-Bash-tools
 pwd
+echo "./DevOps-Bash-tools/lib/utils.sh"
 ./DevOps-Bash-tools/lib/utils.sh
 # shellcheck disable=SC2034,SC2154
 usage_description="
